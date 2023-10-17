d26b0aa8fbfb3e309aed4055af81d3fe517a68d4
shraj15 <145052697+shraj15@users.noreply.github.com>
2023-10-17 14:30:27 -0400

    Add files via upload
    Re-uploaded after fixing the sizing issue. Earlier the whole form was not getting displayed and we had to manually drag and re-size the whole form to display all the contents. Hope this works.

---
+++ Patient.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.awt.Image;
+import java.util.Date;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author shrajnashetty
+ */
+public class Patient {
+    String firstName ;
+    String lastName;
+    String age;
+    String email;
+    String gender;
+    String message;
+    String type;
+    Date dateOfBirth;
+    String imagePath;
+
+
+    public String getfirstName() {
+        return firstName;
+    }
+
+    public void setfirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getlastName() {
+        return lastName;
+    }
+
+    public void setlastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getage() {
+        return age;
+    }
+
+    public void setage(String age) {
+        this.age = age;
+    }
+
+    public String getemail() {
+        return email;
+    }
+
+    public void setemail(String email) {
+        this.email = email;
+    }
+
+    public String getgender() {
+        return gender;
+    }
+
+    public void setgender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getmessage() {
+        return message;
+    }
+
+    public void setmessage(String message) {
+        this.message = message;
+    }
+
+    public String gettype() {
+        return type;
+    }
+
+    public void settype(String type) {
+        this.type = type;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+}
